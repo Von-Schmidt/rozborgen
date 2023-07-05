930b7692a8a496e50392827eb25cc454030973ce
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-05 23:44:52 +0200

    restructure

---
+++ src/routes/Counter.svelte
@@ -0,0 +1,43 @@
+<script>
+	let package_name = '';
+
+	let result = '';
+  
+	async function callAPI() {
+		const response = await fetch('http://localhost:5000/api', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			string_input: 'marysa'
+		})
+		});
+
+		if (!response.ok) {
+		// Handle the error
+		console.error('There was an error with the request');
+		} else {
+		const data = await response.json();
+		result = data.result;
+		}
+  	}
+
+	$: if (result) {
+		alert(result);
+	}
+
+</script>
+	
+<input bind:value={package_name}>
+
+<button on:click={callAPI}>hi</button>
+
+<style>
+	input {
+		border: none;
+		border-bottom: 2px solid;
+		background-color: none;
+	}
+
+</style>
