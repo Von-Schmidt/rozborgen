5636f6223cab4970aaf2c7b936c2de6b9f223ac4
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-07 13:27:56 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>ROZBORGEN</title>
+	<meta name="description" content="Generátor rozborů z maturitní četby" />
 </svelte:head>
 
 <section>
@@ -28,19 +28,4 @@
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
