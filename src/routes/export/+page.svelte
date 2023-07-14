d7fed81aca122133bcfb4efe09a4b7f491c68997
Vincent Šmíd <45830825+Von-Schmidt@users.noreply.github.com>
2023-07-14 18:16:50 +0200

    Style conservation
    Added styling to export +page, moved it from Explanation.svelte to the global file.

--- src/routes/export/+page.svelte
+++ src/routes/export/+page.svelte
@@ -1,8 +1,15 @@
 <script>
     import { resultglobal } from '../stores';
+    import '../styles.css';
 
     $: console.log($resultglobal);
 </script> 
 
 
-<p>{@html $resultglobal}</p>
+<p></p>
+
+<div class="explanationwrapper">
+    <div class="explanation">
+        <p class="explanationtext">{@html $resultglobal}</p>
+    </div>
+</div>
